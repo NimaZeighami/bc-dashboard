d6a5439a17ccdd3c827dec4fb43879d2ffb280b9
Nima Zeighami <nimazeighamy1382@gmail.com>
2025-05-17 10:37:07 +0330

    initialize the project

---
+++ src/config-global.ts
@@ -0,0 +1,11 @@
+import packageJson from '../package.json';
+
+// ----------------------------------------------------------------------
+
+export type ConfigValue = {
+  appName: string;
+};
+
+export const CONFIG: ConfigValue = {
+  appName: 'BC Platform',
+};
